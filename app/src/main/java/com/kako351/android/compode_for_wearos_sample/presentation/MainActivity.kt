17b97ef852ee4c2c85eba4e588426a522d508519
kako351 <h3.birth@gmail.com>
2022-07-11 01:17:41 +0900

    add TopScreen

--- app/src/main/java/com/kako351/android/compode_for_wearos_sample/presentation/MainActivity.kt
+++ app/src/main/java/com/kako351/android/compode_for_wearos_sample/presentation/MainActivity.kt
@@ -24,46 +24,15 @@ import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.kako351.android.compode_for_wearos_sample.R
 import com.kako351.android.compode_for_wearos_sample.presentation.theme.CompodeforwearossampleTheme
+import com.kako351.android.compode_for_wearos_sample.presentation.ui.screen.TopScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+            CompodeforwearossampleTheme {
+                TopScreen()
+            }
         }
     }
-}
-
-@Composable
-fun WearApp(greetingName: String) {
-    CompodeforwearossampleTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
-         */
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Greeting(greetingName = greetingName)
-        }
-    }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
 }
---
+++ app/src/main/java/com/kako351/android/compode_for_wearos_sample/presentation/ui/screen/TopScreen.kt
@@ -0,0 +1,55 @@
+package com.kako351.android.compode_for_wearos_sample.presentation.ui.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.*
+import com.kako351.android.compode_for_wearos_sample.presentation.theme.CompodeforwearossampleTheme
+import com.kako351.android.compode_for_wearos_sample.presentation.ui.components.AppCardExample
+
+@OptIn(ExperimentalWearMaterialApi::class)
+@Composable
+fun TopScreen (){
+    val listState = rememberScalingLazyListState()
+    Scaffold(
+        timeText = {
+            if (!listState.isScrollInProgress) {
+                TimeText()
+            }
+        },
+        vignette = {
+            Vignette(vignettePosition = VignettePosition.TopAndBottom)
+        },
+        positionIndicator = {
+            PositionIndicator(
+                scalingLazyListState = listState
+            )
+        }
+    ) {
+        val contentModifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+        val iconModifier = Modifier
+            .size(24.dp)
+            .wrapContentSize(align = Alignment.Center)
+        ScalingLazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            autoCentering = AutoCenteringParams(itemIndex = 0),
+            state = listState
+        ) {
+            item { AppCardExample() }
+        }
+    }
+}
+
+@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
+@Composable
+fun PreviewTopScreen() {
+    CompodeforwearossampleTheme {
+        TopScreen()
+    }
+}
